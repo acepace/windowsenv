0eca4d4fae38d725e6dbf76c6c59ee6767bcb717
Ace Pace <acepace@users.noreply.github.com>
2022-02-26 20:26:11 +0200

    Update terminalsetup.ps1

--- terminalsetup.ps1
+++ terminalsetup.ps1
@@ -1,3 +1,4 @@
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
+install-module DockerCompletion -Scope CurrentUser
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
