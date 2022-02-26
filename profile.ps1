cae11b1ae092e1a3390dd3e48540313865bd7e9b
Ace Pace <acepace@users.noreply.github.com>
2022-02-26 20:47:20 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,10 @@
 Import-Module posh-git
 Import-Module oh-my-posh
+Import-Module DockerCompletion
 Set-Theme Operator
+
+C:\ProgramData\chocolatey\lib\Bat\tools\*\autocomplete\_bat.ps1
+C:\ProgramData\chocolatey\lib\ripgrep\tools\*\complete\_rg.ps1
 
 New-Alias npp 'C:\Program Files\Notepad++\notepad++.exe'
 New-Alias 7z 'C:\Program Files\7-Zip\7z.exe'
